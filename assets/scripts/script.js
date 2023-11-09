c37ff3f1e5b51aa8ca7d73d09e6ea73a1f34d640
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-09 06:22:29 -0300

    Unfortunately I erased all my code... So I copyied and pasted the laste version. I must to study more about git

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,11 @@
-alert('The script is running!')
+const spanEl = document.querySelector('#spanEl')
+
+document.addEventListener("click", (e) => {
+  let X = e.pageX;
+  let Y = e.pageY;
+
+  spanEl.style.width = "10rem"
+  spanEl.style.height = "10rem"
+  spanEl.style.setProperty("--Xposition", `${X}px`)  
+  spanEl.style.setProperty("--Yposition", `${Y}px`)  
+})
