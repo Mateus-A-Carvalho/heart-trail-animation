24538eaab9d72a045c6b721e2583ce58eed62724
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-08 21:30:12 -0300

    got the X and Y position of mouse and set property to span appear when event 'click' trigged.

---
+++ assets/scripts/script.js
@@ -0,0 +1,11 @@
+const spanEl = document.querySelector('#spanEl')
+
+document.addEventListener("click", (e) => {
+  let X = e.pageX;
+  let Y = e.pageY;
+
+  spanEl.style.width = "10rem"
+  spanEl.style.height = "10rem"
+  spanEl.style.setProperty("--Xposition", `${X}px`)  
+  spanEl.style.setProperty("--Yposition", `${Y}px`)  
+})
