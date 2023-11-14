009931e527d3e3ad030cf09e9e3ad1ea2337ae7f
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-14 12:52:10 -0300

    I fixed the problem of Xposition. Was a declaration in keyframes that didn't let the X position changes.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,11 +6,14 @@ document.addEventListener("mousemove", (e) => {
   const Y = e.offsetY;
 
   const spanEl = document.createElement("span");
+  bodyEl.appendChild(spanEl);
 
   spanEl.style.top = `${Y}px`;
   spanEl.style.left = `${X}px`;
+
+  console.log(spanEl.style.top, spanEl.style.left);
+
   spanEl.style.width = "10rem"
   spanEl.style.height = "10rem"
 
-  bodyEl.appendChild(spanEl);
 })
